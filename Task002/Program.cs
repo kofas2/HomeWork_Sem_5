86b2900374cae94de51ffb575da42df193d4de48
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-25 22:31:58 +0300

    add Task002 Home work 5

---
+++ Task002/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10,100);
+    }
+}
+
+void PrintArray(int []array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+void SumNegativeNum(int [] array)
+{
+    int sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i%2!=0)
+        {
+            sumNegative += array[i];
+        }
+
+    }
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях, равна: {sumNegative}");
+}
+
+
+
+
+Console.WriteLine("Введите количество значений массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+FillArray(array);
+PrintArray(array);
+SumNegativeNum(array);
