e1bbedf14f1f91969f5f66ee3b532da7c5e46eb4
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-25 23:17:46 +0300

    add Task003 Home work 5

---
+++ Task003/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+void FillArray(float [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10,100);
+    }
+}
+
+void PrintArray(float []array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+void MaxMinDifference(float [] array)
+{
+    float max = float.MinValue;
+    float min = float.MaxValue;
+    float sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    sum = max-min;
+    Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {sum}");
+}
+
+float [] array = new float [5];
+FillArray(array);
+PrintArray(array);
+MaxMinDifference(array);
